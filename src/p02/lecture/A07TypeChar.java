65d9b59b85798d32d1a109ee0736f365ec3e5e34
jean5694 <jeanjean5694@gmail.com>
2021-03-25 16:33:04 +0900

    ch02

---
+++ src/p02/lecture/A07TypeChar.java
@@ -0,0 +1,32 @@
+package p02.lecture;
+
+public class A07TypeChar {
+	public static void main(String[] args) {
+		//cahr : 2byte (8bit) 문자저장
+		// javascript
+		//var a = "a";
+		//	  a = "가";
+		
+		// java 문자열
+		String s1 = "hello"; //2자 이상 문자열을 쓸 때
+		s1 = "자바"; // 큰 따옴표 string literal
+		s1 = "자";
+		s1 = "a";
+		s1 ="";
+		
+		// java 문자
+		char charVar;
+//		charVar = "a"; // not ok 
+		charVar = 'a'; // 작은 따옴표 char literal
+		charVar = '눈';
+		System.out.println(charVar);
+		charVar = '\uB208';
+		System.out.println(charVar);
+		charVar = 45576;
+		System.out.println(charVar);
+		
+//		charVar = ''; //not ok
+		charVar = '\u10E6';
+		System.out.println(charVar);
+	}
+}
---
+++ src/p02/lecture/A07TypeCharEx1.java
@@ -0,0 +1,21 @@
+package p02.lecture;
+
+public class A07TypeCharEx1 {
+	public static void main(String[] args) {
+		String str = "lorem, ipsum, JAVA";
+		
+		int count =0;
+		for (int i = 0 ; i <str.length(); i++) {
+			char c = str.charAt(i);
+//			if ((97 < c && c < 122)||(65 < c && c < 90)) {
+				
+				
+				if(('a' <=c && c <= 'z')||('A' <= c && c < 'Z')) {
+					count++;
+				}
+			} 
+			System.out.println(count);
+
+		}
+		}
+
