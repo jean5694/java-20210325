f3f8dd48209e376ade57604def277f4f52cf68f7
jean5694 <jeanjean5694@gmail.com>
2021-03-31 15:20:21 +0900

    ch04

--- src/p04/lecture/A12ForPrimeNumber.java
+++ src/p04/lecture/A12ForPrimeNumber.java
@@ -6,22 +6,22 @@ public class A12ForPrimeNumber {
 	public static void main(String[] args) {
 		// 소수 (prime number) : 약수가 1과 자기자신
 		// ex) 2, 3, 5, 7, 11, 13
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("수 입력>");
 		int number = scanner.nextInt();
-		
+
 		System.out.println("입력한 수 : " + number);
-		
+
 		for (int i = 2; i < number; i++) {
-			if ((number % i) == 0 ) {
+			if ((number % i) == 0) {
 				System.out.println("소수가 아닙니다.");
+				scanner.close();
 				return;
 			}
 		}
-		
 		System.out.println("소수 입니다.");
-		return;
+
 	}
 }
