0922cf0be1692e30547aea4d79a4ed4c81eb4f64
jean5694 <jeanjean5694@gmail.com>
2021-04-02 10:12:33 +0900

    ch05

---
+++ src/p05/lecture/A09Null.java
@@ -0,0 +1,18 @@
+package p05.lecture;
+
+public class A09Null {
+	public static void main(String[] args) {
+		//Null : 참조타입이 없음
+		
+		String s = "java"; // s : 999
+		String t = "java"; // t : 999
+		
+		t = null;  // 참조하는 객체(instance)가 없음
+		// null로 인해 999참조값이 사라져 t는 참조값이 없다
+		s = null;
+		// 두 가지 모두 null 로 참조값을 없앤 경우,
+		
+				
+		
+	}
+}
