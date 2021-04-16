b6800dd590cc109b4e4bac35812b340b0fe8842b
jean5694 <jeanjean5694@gmail.com>
2021-04-16 21:25:22 +0900

    ch09

--- src/p09/lecture/A01NestedClassExample.java
+++ src/p09/lecture/A01NestedClassExample.java
@@ -2,10 +2,10 @@ package p09.lecture;
 
 public class A01NestedClassExample {
 	public static void main(String[] args) {
-		A01NestedClass.StaticClass o1 = new A01NestedClass.StaticClass();
-//		A01NestedClass.InnerClass o2 = new A01NestedClass.InnerClass();
+		A01NestedClass.StaticClass o1 = new A01NestedClass.StaticClass(); //스태틱은 가능
+//		A01NestedClass.InnerClass o2 = new A01NestedClass.InnerClass(); // 인스턴스는 불가능
 		
-		A01NestedClass outer1 = new A01NestedClass();
+		A01NestedClass outer1 = new A01NestedClass(); //인스턴스는 객체형성을 먼저 해줘야 함.
 		A01NestedClass.InnerClass o3 = outer1.new InnerClass();
 	}
 }
