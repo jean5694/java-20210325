c0969e949f52dd6bc254683a953f951fac0c4177
jean5694 <jeanjean5694@gmail.com>
2021-04-23 19:00:30 +0900

    ch12

--- src/p12/lecture/A12Concurrency.java
+++ src/p12/lecture/A12Concurrency.java
@@ -1,8 +1,11 @@
 package p12.lecture;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class A12Concurrency {
 
 	static long shareValue = 0;
+	static AtomicInteger sv = new AtomicInteger();
 	
 	public static void main(String[] args) {
 		Thread t = new Thread(new Runnable() {
@@ -10,17 +13,25 @@ public class A12Concurrency {
 			public void run() {
 				for (int i = 0; i < 100_0000; i++) {
 					shareValue++;
+					sv.incrementAndGet();
 				}
 			}
 		});
 		t.start();
+		
+		for (int i = 0 ; i < 100_0000; i++) {
+			shareValue++;
+			sv.incrementAndGet();
+		}
+		
 		try {
 			t.join();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		System.out.println(shareValue);
+		System.out.println(sv.get());
 		
 	}
 }
