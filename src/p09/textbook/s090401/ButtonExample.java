92991757d439fa6036092134388ab5e3a52e88f1
jean5694 <jeanjean5694@gmail.com>
2021-04-29 12:15:34 +0900

    ch09

--- src/p09/textbook/s090401/ButtonExample.java
+++ src/p09/textbook/s090401/ButtonExample.java
@@ -3,11 +3,21 @@ package p09.textbook.s090401;
 public class ButtonExample {
 	public static void main(String[] args) {
 		Button btn = new Button();
-		
+
 		btn.setOnClickListener(new CallListener());
 		btn.touch();
-		
+
 		btn.setOnClickListener(new MessageListener());
+		btn.touch();
+
+		btn.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick() {
+				System.out.println("사진을 찍습니다.");
+			}
+			
+		});
 		btn.touch();
 	}
 }
