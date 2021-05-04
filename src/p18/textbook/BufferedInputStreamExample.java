33e49a8bd4c0ab4cd4b4c750663628755b4a4028
jean5694 <jeanjean5694@gmail.com>
2021-05-04 11:32:51 +0900

    CH18

--- src/p18/textbook/BufferedInputStreamExample.java
+++ src/p18/textbook/BufferedInputStreamExample.java
@@ -7,26 +7,25 @@ public class BufferedInputStreamExample {
 	public static void main(String[] args) throws Exception {
 		long start = 0;
 		long end = 0;
-		
+
 		String file = "src/p18/textbook/BufferedInputStreamExample.java";
 		FileInputStream fis1 = new FileInputStream(file);
-		start = System.currentTimeMillis();
-		
-		while(fis1.read() != -1) {
-			
+		start = System.nanoTime();
+
+		while (fis1.read() != -1) {
 		}
-		
-		end = System.currentTimeMillis();
-		System.out.println("사용하지 않았을 때: " + (end-start) +"ms");
+
+		end = System.nanoTime();
+		System.out.println("사용하지 않았을 때: " + (end - start) + "ns");
 		fis1.close();
-		
-	
+
 		FileInputStream fis2 = new FileInputStream(file);
 		BufferedInputStream bis = new BufferedInputStream(fis2);
-		start = System.currentTimeMillis();
-		while(bis.read() != -1) {}
-		end = System.currentTimeMillis();
-		System.out.println("사용했을 때: " +(end-start) + "ms");
+		start = System.nanoTime();
+		while (bis.read() != -1) {
+		}
+		end = System.nanoTime();
+		System.out.println("사용했을 때: " + (end - start) + "ns");
 		bis.close();
 		fis2.close();
 	}
